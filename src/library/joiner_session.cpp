10c24b4991187a75768aee900c07d39565f25058
kangping <wgtdkp@google.com>
2020-09-02 12:04:09 +0800

    fix logging format

--- src/library/joiner_session.cpp
+++ src/library/joiner_session.cpp
@@ -180,7 +180,8 @@ void JoinerSession::HandleJoinFin(const coap::Request &aJoinFin)
              vendorSwVersionTlv->GetValueAsString(), utils::Hex(vendorStackVersionTlv->GetValue()), provisioningUrl,
              utils::Hex(vendorData));
 
-    LOG_INFO(LOG_REGION_THCI, "session(={}) received JOIN_FIN.req: {}", utils::Hex(aJoinFin.GetPayload()));
+    LOG_INFO(LOG_REGION_THCI, "session(={}) received JOIN_FIN.req: {}", static_cast<void *>(this),
+             utils::Hex(aJoinFin.GetPayload()));
 
     // Validation done, request commissioning by user.
     accepted = mCommImpl.mCommissionerHandler.OnJoinerFinalize(
@@ -210,7 +211,8 @@ void JoinerSession::SendJoinFinResponse(const coap::Request &aJoinFinReq, bool a
     LOG_INFO(LOG_REGION_JOINER_SESSION, "session(={}) sent JOIN_FIN.rsp: accepted={}", static_cast<void *>(this),
              aAccept);
 
-    LOG_INFO(LOG_REGION_THCI, "session(={}) sent JOIN_FIN.rsp: {}", utils::Hex(joinFin.GetPayload()));
+    LOG_INFO(LOG_REGION_THCI, "session(={}) sent JOIN_FIN.rsp: {}", static_cast<void *>(this),
+             utils::Hex(joinFin.GetPayload()));
 
 exit:
     if (error != ErrorCode::kNone)
