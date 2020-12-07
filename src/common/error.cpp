cdc74719abdb30d7624ade4cf89aa7c031fd67a1
igor.vartanov <igor.vartanov@dsr-corporation.com>
2020-12-07 11:04:53 +0300

    fix missing error code to string conversion

--- src/common/error.cpp
+++ src/common/error.cpp
@@ -80,6 +80,8 @@ static std::string ErrorCodeToString(ErrorCode code)
         return "REJECTED";
     case ErrorCode::kCoapError:
         return "COAP_ERROR";
+    case ErrorCode::kSyntaxViolation:
+        return "SYNTAX_VIOLATION";
     case ErrorCode::kUnknown:
         return "UNKNOWN";
 
