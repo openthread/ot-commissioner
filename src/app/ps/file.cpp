d8e6555856554fabee33b8ba0ed7ea61551f1419
Mikhail Pridushchenko <mikhail.pridushchenko@dsr-corporation.com>
2020-12-09 10:47:30 +0300

    Implement 'no persistence' and JSON path reconstruction
    - no file persistence but in-memory operations only for JSON persistent
      storage if JSON path is empty.
    - attempt to create all necessary directories if JSON path does not exist

--- src/app/ps/file.cpp
+++ src/app/ps/file.cpp
@@ -1,19 +1,11 @@
 #include "file.hpp"
 
-#if defined(_WIN32) || defined(WIN32)
-
-#include <fcntl.h>
-#include <io.h>
-#include <share.h>
-
-#else
-
 #include <sys/file.h>
 #include <unistd.h>
 
-#endif
-
 #include <errno.h>
+
+#include <memory>
 
 namespace ot {
 
@@ -21,64 +13,96 @@ namespace os {
 
 namespace file {
 
+namespace {
+
+file_status open_error(int err)
+{
+    if (err == ENOENT)
+    {
+        return FS_NOT_EXISTS;
+    }
+    else if (err == EACCES)
+    {
+        return FS_PERMISSION_ERROR;
+    }
+    return FS_ERROR;
+}
+
+const std::string &&basename(const std::string &path)
+{
+    std::string tmp_path = path;
+    // Special pathes: "/", "..", "."
+    while (tmp_path.size() && tmp_path.back() == '/')
+    {
+        tmp_path.pop_back();
+    }
+    auto pos = tmp_path.find_last_of('/');
+    if (pos != std::string::npos)
+    {
+        return std::move(tmp_path.substr(0, pos));
+    }
+    return std::move(std::string());
+}
+
+file_status restore_path_recursive(const std::string &rel_dir)
+{
+    (void)rel_dir;
+    return FS_ERROR;
+}
+
+} // namespace
+
 file_status file_read(std::string const &name, std::string &data)
 {
-    int fd = 0;
-#if defined(_WIN32) || defined(WIN32)
-    fd = _sopen(name.c_str(), _O_RDONLY, _SH_DENYRW, _S_IREAD | _S_IWRITE);
-#else
-    fd = open(name.c_str(), O_RDONLY, S_IWUSR | S_IRUSR);
-#endif
+    int fd = open(name.c_str(), O_RDONLY, S_IWUSR | S_IRUSR);
     if (fd == -1)
     {
-        if (errno == ENOENT)
+        if (errno != ENOENT)
         {
-            return FS_NOT_EXISTS;
+            return open_error(errno);
         }
-        else if (errno == EACCES)
+        fd = creat(name.c_str(), 0660);
+        if (fd == -1)
         {
-            return FS_PERMISSION_ERROR;
+            if (errno != ENOENT)
+            {
+                return open_error(errno);
+            }
+            std::string containing_dir = basename(name);
+            file_status result         = restore_path_recursive(containing_dir);
+            if (result != FS_SUCCESS)
+            {
+                return result;
+            }
+            fd = creat(name.c_str(), 0660);
+            if (fd == -1)
+            {
+                return open_error(errno);
+            }
         }
-        return FS_ERROR;
     }
 
-#if !defined(_WIN32) && !defined(WIN32)
     if (flock(fd, LOCK_EX) == -1)
     {
         close(fd);
         return FS_ERROR;
     }
-#endif
 
     char buff[10 * 1024] = {0};
     int  read_bytes      = 0;
-#if defined(_WIN32) || defined(WIN32)
-    while ((read_bytes = _read(fd, buff, sizeof(buff) - 1)) > 0)
-    {
-#else
     while ((read_bytes = read(fd, buff, sizeof(buff) - 1)) > 0)
     {
-#endif
         buff[read_bytes] = 0;
         data += std::string(buff);
     }
 
-#if defined(_WIN32) || defined(WIN32)
-    _close(fd);
-#else
     close(fd);
-#endif
     return FS_SUCCESS;
 }
 
 file_status file_write(std::string const &name, std::string const &data)
 {
-    int fd = 0;
-#if defined(_WIN32) || defined(WIN32)
-    fd = _sopen(name.c_str(), _O_CREAT | _O_WRONLY | _O_TRUNC, _SH_DENYRW, _S_IREAD | _S_IWRITE);
-#else
-    fd = open(name.c_str(), O_CREAT | O_WRONLY | O_TRUNC, S_IWUSR | S_IRUSR);
-#endif
+    int fd = open(name.c_str(), O_CREAT | O_WRONLY | O_TRUNC, S_IWUSR | S_IRUSR);
     if (fd == -1)
     {
         if (errno == EACCES)
@@ -88,21 +112,14 @@ file_status file_write(std::string const &name, std::string const &data)
         return FS_ERROR;
     }
 
-#if !defined(_WIN32) && !defined(WIN32)
     if (flock(fd, LOCK_EX) == -1)
     {
         close(fd);
         return FS_ERROR;
     }
-#endif
 
-#if defined(_WIN32) || defined(WIN32)
-    _write(fd, data.c_str(), data.size());
-    _close(fd);
-#else
     write(fd, data.c_str(), data.size());
     close(fd);
-#endif
     return FS_SUCCESS;
 }
 
