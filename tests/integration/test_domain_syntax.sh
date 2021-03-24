ea8ed6cc86e453cbf4a43b42f6bfa86ac6d2cd0c
Mikhail Pridushchenko <mikhail.pridushchenko@dsr-corporation.com>
2021-03-24 19:44:37 +0300

    Fix multi-network syntax integration tests

--- tests/integration/test_domain_syntax.sh
+++ tests/integration/test_domain_syntax.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+<#!/bin/bash
 #
 #  Copyright (c) 2021, The OpenThread Commissioner Authors.
 #  All rights reserved.
@@ -29,35 +29,39 @@
 
 [ -z "${TEST_ROOT_DIR}" ] && . "$(dirname "$0")"/common.sh
 
+mdns_announce_add_th() {
+	sudo cp ${CUR_DIR}/etc/test_harness/* /etc/avahi/services/
+	sleep 3
+	send_command_to_commissioner "br scan --timeout 1000 --export /tmp/nwk.json"
+	sed -i '/Addr/ s/^.*$/"Addr": "::1",/' /tmp/nwk.json
+	send_command_to_commissioner "br add /tmp/nwk.json"
+	rm -f /tmp/nwk.json
+	sudo rm -f /etc/avahi/services/*.service
+}
+
 test_network_list() {
     set -e
 
-	install_borderagent_mdns_data etc/br_scan_initial
     start_border_agent_mdns_service
     start_commissioner "${NON_CCM_CONFIG}"
+	mdns_hosts_map_addresses
+	commissioner_mdns_scan_import ${CUR_DIR}/etc/br_scan_initial
 
-    send_command_to_commissioner "br scan --export /tmp/br_scan_export.json"
-	send_command_to_commissioner "br add /tmp/br_scan_export.json"
 	send_command_to_commissioner "network list" "thread1"
 	
     stop_commissioner
+	mdns_hosts_unmap_addresses
     stop_border_agent_mdns_service
-	uninstall_borderagent_mdns_data etc/br_scan_initial
-
-	rm -f /tmp/br_scan_export.json
 }
 
 test_select_identify() {
     set -e
 
-	rm -f /tmp/br_scan_export.json /tmp/registry.json
-
-	install_borderagent_mdns_data etc/br_scan_initial
     start_border_agent_mdns_service
     start_commissioner "${NON_CCM_CONFIG}"
+	mdns_hosts_map_addresses
+	commissioner_mdns_scan_import ${CUR_DIR}/etc/br_scan_initial
 
-    send_command_to_commissioner "br scan --export /tmp/br_scan_export.json"
-	send_command_to_commissioner "br add /tmp/br_scan_export.json"
 	send_command_to_commissioner "network identify" "none"
 	send_command_to_commissioner "network select thread1"
 	send_command_to_commissioner "network identify" "thread1"
@@ -69,103 +73,176 @@ test_select_identify() {
 	send_command_to_commissioner "network identify" "none"
 
     stop_commissioner
+	mdns_hosts_unmap_addresses
     stop_border_agent_mdns_service
-	uninstall_borderagent_mdns_data etc/br_scan_initial
-
-	rm -f /tmp/br_scan_export.json
 }
 
 test_start_stop_selected() {
     set -e
 
-	install_borderagent_mdns_data etc/br_scan_initial
     start_border_agent_mdns_service
-    start_commissioner "${NON_CCM_CONFIG}"
 
-    send_command_to_commissioner "br scan --export /tmp/br_scan_export.json"
-	send_command_to_commissioner "br add /tmp/br_scan_export.json"
+	start_daemon
+	form_network "${PSKC}"
+
+	start_commissioner "${NON_CCM_CONFIG}"
+
+	mdns_announce_add_th
+
 	send_command_to_commissioner "network identify" "none"
-	send_command_to_commissioner "network select thread-net1"
-	send_command_to_commissioner "start" "[done]"
-	send_command_to_commissioner "opdataset get active" "thread-net1"
+	send_command_to_commissioner "network select openthread-test"
+	send_command_to_commissioner "start"
+	send_command_to_commissioner "opdataset get active" "openthread-test"
 	send_command_to_commissioner "stop"
 	send_command_to_commissioner "network select none"
 	send_command_to_commissioner "start --dom TestDomainName"
-	send_command_to_commissioner "opdataset get active" "thread-net1"
+	send_command_to_commissioner "opdataset get active --dom TestDomainName" "openthread-test"
 	send_command_to_commissioner "stop --nwk all"
 
     stop_commissioner
+	stop_daemon
     stop_border_agent_mdns_service
-	uninstall_borderagent_mdns_data etc/br_scan_initial
-
-	rm -f /tmp/br_scan_export.json
 }
 
-test_start_stop_mn() {
+test_start_stop_mn_explicit() {
     set -e
 
 	aods_fn="/tmp/aods.json"
 	
-	install_borderagent_mdns_data etc/br_scan_initial
     start_border_agent_mdns_service
+
+	start_daemon
+	form_network "${PSKC}"
+
     start_commissioner "${NON_CCM_CONFIG}"
 
-    send_command_to_commissioner "br scan --export /tmp/br_scan_export.json"
-	send_command_to_commissioner "br add /tmp/br_scan_export.json"
+	mdns_announce_add_th
 
-	send_command_to_commissioner "start --nwk thread-net1"
-	send_command_to_commissioner "opdataset get active --nwk thread-net1 --export $aods_fn"
-	send_command_to_commissioner "stop --nwk thread-net1"
-	send_command_to_commissioner "active --nwk thread-net1" "false"
+	mdns_hosts_map_addresses
+	commissioner_mdns_scan_import ${CUR_DIR}/etc/br_scan_initial
+
+	send_command_to_commissioner "start --nwk openthread-test"
+	send_command_to_commissioner "opdataset get active --nwk openthread-test --export $aods_fn"
+	send_command_to_commissioner "stop --nwk openthread-test"
+	send_command_to_commissioner "active --nwk openthread-test" "false"
+
+    stop_commissioner
+	stop_daemon
+	mdns_hosts_unmap_addresses
+    stop_border_agent_mdns_service
+}
+
+test_start_stop_mn_all() {
+    set -e
+
+	aods_fn="/tmp/aods.json"
+	
+    start_border_agent_mdns_service
+
+	start_daemon
+	form_network "${PSKC}"
+
+    start_commissioner "${NON_CCM_CONFIG}"
+
+	mdns_announce_add_th
+
+	mdns_hosts_map_addresses
+	commissioner_mdns_scan_import ${CUR_DIR}/etc/br_scan_initial
 
 	send_command_to_commissioner "start --nwk all"
-	send_command_to_commissioner "active --nwk all" '2222222222222222.*false'
-	send_command_to_commissioner "sessionid --nwk all" '1111111111111111.*nil'
-	send_command_to_commissioner "opdataset get active --nwk all" '0000000000000001.*\{'
-	send_command_to_commissioner "commdataset get --nwk all" '3333333333333333.*nil'
+	send_command_to_commissioner "active --nwk all" 'DEAD00BEEF00CAFE.*true'
+	send_command_to_commissioner "sessionid --nwk all" 'DEAD00BEEF00CAFE": \d*'
+	send_command_to_commissioner "opdataset get active --nwk all" 'DEAD00BEEF00CAFE": {'
+	send_command_to_commissioner "commdataset get --nwk all" 'DEAD00BEEF00CAFE": {'
 # TODO issue #29
 #	send_command_to_commissioner "opdataset set securitypolicy 1000 ff --nwk all"
 	send_command_to_commissioner "opdataset get pending --nwk all"
 	send_command_to_commissioner "stop --nwk all"
 
+    stop_commissioner
+	stop_daemon
+	mdns_hosts_unmap_addresses
+    stop_border_agent_mdns_service
+}
+
+test_start_stop_mn_other() {
+    set -e
+
+	aods_fn="/tmp/aods.json"
+	
+    start_border_agent_mdns_service
+
+	start_daemon
+	form_network "${PSKC}"
+
+    start_commissioner "${NON_CCM_CONFIG}"
+
+	mdns_announce_add_th
+
+	mdns_hosts_map_addresses
+	commissioner_mdns_scan_import ${CUR_DIR}/etc/br_scan_initial
+
 	send_command_to_commissioner "network select 2222222222222222"
 	send_command_to_commissioner "start --nwk other"
-	send_command_to_commissioner "active --nwk other" '2222222222222222.*false'
-	send_command_to_commissioner "sessionid --nwk other" '1111111111111111.*nil'
-	send_command_to_commissioner "opdataset get active --nwk other" '0000000000000001.*\{'
-	send_command_to_commissioner "commdataset get --nwk other" '3333333333333333.*nil'
+	send_command_to_commissioner "active --nwk other" '1111111111111111": false'
+	send_command_to_commissioner "sessionid --nwk other" 'DEAD00BEEF00CAFE": \d*'
+	send_command_to_commissioner "opdataset get active --nwk other" 'DEAD00BEEF00CAFE": {'
+	send_command_to_commissioner "commdataset get --nwk other" 'DEAD00BEEF00CAFE": {'
 # TODO issue #29
 #	send_command_to_commissioner "opdataset set securitypolicy 1000 ff --nwk other"
 	send_command_to_commissioner "opdataset get pending --nwk other"
 	send_command_to_commissioner "stop --nwk other"
 	send_command_to_commissioner "network select none"
+
+    stop_commissioner
+	stop_daemon
+	mdns_hosts_unmap_addresses
+    stop_border_agent_mdns_service
+}
+
+test_start_stop_mn_dom() {
+    set -e
+
+	aods_fn="/tmp/aods.json"
 	
+    start_border_agent_mdns_service
+
+	start_daemon
+	form_network "${PSKC}"
+
+    start_commissioner "${NON_CCM_CONFIG}"
+
+	mdns_announce_add_th
+
+	mdns_hosts_map_addresses
+	commissioner_mdns_scan_import ${CUR_DIR}/etc/br_scan_initial
+
+	set -x
 	send_command_to_commissioner "start --dom TestDomainName"
-	send_command_to_commissioner "active --dom TestDomainName" '2222222222222222.*false'
-	send_command_to_commissioner "sessionid --dom TestDomainName" '1111111111111111.*nil'
-	send_command_to_commissioner "opdataset get active --dom TestDomainName" '0000000000000001.*\{'
-	send_command_to_commissioner "commdataset get --dom TestDomainName" '3333333333333333.*nil'
+	send_command_to_commissioner "active --dom TestDomainName" '2222222222222222": false'
+	send_command_to_commissioner "sessionid --dom TestDomainName" 'DEAD00BEEF00CAFE": \d*'
+	send_command_to_commissioner "opdataset get active --dom TestDomainName" 'DEAD00BEEF00CAFE": {'
+	send_command_to_commissioner "commdataset get --dom TestDomainName" 'DEAD00BEEF00CAFE": {'
 # TODO issue #29
 #	send_command_to_commissioner "opdataset set securitypolicy 1000 ff --dom TestDomainName"
 	send_command_to_commissioner "opdataset get pending --dom TestDomainName"
 	send_command_to_commissioner "stop --dom TestDomainName"
 
-    stop_commissioner
-    stop_border_agent_mdns_service
-	uninstall_borderagent_mdns_data etc/br_scan_initial
+	set +x
 
-	rm -f /tmp/br_scan_export.json
+    stop_commissioner
+	stop_daemon
+	mdns_hosts_unmap_addresses
+    stop_border_agent_mdns_service
 }
 
-test_network_management() {
+_network_management() {
     set -e
 
-	install_borderagent_mdns_data etc/br_scan_initial
     start_border_agent_mdns_service
     start_commissioner "${NON_CCM_CONFIG}"
-
-    send_command_to_commissioner "br scan --export /tmp/br_scan_export.json"
-	send_command_to_commissioner "br add /tmp/br_scan_export.json"
+	mdns_hosts_map_addresses
+	commissioner_mdns_scan_import ${CUR_DIR}/etc/br_scan_initial
 
 	send_command_to_commissioner "network list --nwk thread1" "1111111111111111"
 	send_command_to_commissioner "network list --dom TestDomainName" "2222222222222222"
@@ -201,17 +278,13 @@ test_network_management() {
 	send_command_to_commissioner "domain list --dom TestDomainName" '\[failed\]'
 	
     stop_commissioner
+	mdns_hosts_unmap_addresses
     stop_border_agent_mdns_service
-	uninstall_borderagent_mdns_data etc/br_scan_initial
-
-	rm -f /tmp/br_scan_export.json
 }
 
 test_mn_input_data() {
     set -e
 
-	install_borderagent_mdns_data etc/br_scan_initial
-    start_border_agent_mdns_service
     start_commissioner "${NON_CCM_CONFIG}"
 
 	send_command_to_commissioner "network list --nwk unknown-net" '\[failed\]'
@@ -225,25 +298,20 @@ test_mn_input_data() {
 	send_command_to_commissioner "br delete --dom unknown-domain" '\[failed\]'
 	
     stop_commissioner
-    stop_border_agent_mdns_service
-	uninstall_borderagent_mdns_data etc/br_scan_initial
-
-	rm -f /tmp/br_scan_export.json
 }
 
 test_mn_input_data_export_import() {
     set -e
 
+    start_border_agent_mdns_service
+
     start_daemon
     form_network "${PSKC}"
 
-	install_borderagent_mdns_data etc/test_harness
-    start_border_agent_mdns_service
-
     start_commissioner "${NON_CCM_CONFIG}"
 
-	send_command_to_commissioner "br scan --export /tmp/br_scan_export.json"
-	send_command_to_commissioner "br add /tmp/br_scan_export.json"
+	mdns_announce_add_th
+
     send_command_to_commissioner "br list" "addr"
 
 	send_command_to_commissioner "network select openthread-test"
@@ -253,12 +321,32 @@ test_mn_input_data_export_import() {
 	send_command_to_commissioner "opdataset get active --nwk openthread-test --export /tmp/aods1.json"
 	
 	send_command_to_commissioner "stop"
-	
+
     stop_commissioner
-    stop_border_agent_mdns_service
-	uninstall_borderagent_mdns_data etc/test_harness
-
     stop_daemon
+	stop_border_agent_mdns_service
 
-	rm -f /tmp/aods1.json /tmp/aods.json /tmp/br_scan_export.json
+	rm -f /tmp/aods.json
+}
+
+test_start_on_fake_br_data() {
+	set -e
+
+	start_border_agent_mdns_service
+
+	start_daemon
+	form_network "${PSKC}"
+
+	start_commissioner "${NON_CCM_CONFIG}"
+
+	mdns_announce_add_th
+
+	send_command_to_commissioner "network select openthread-test"
+	send_command_to_commissioner "start"
+	send_command_to_commissioner "active" "true"
+	send_command_to_commissioner "stop"
+
+	stop_commissioner
+	stop_daemon
+	stop_border_agent_mdns_service
 }
