3b3d43b99db99b07225993ac36f434919ae7ff9b
ZhangLe2016 <156590889+ZhangLe2016@users.noreply.github.com>
2024-04-24 11:08:36 +0800

    Update src/app/br_discover.cpp
    Co-authored-by: Kangping <wgtdkp@google.com>

--- src/app/br_discover.cpp
+++ src/app/br_discover.cpp
@@ -53,9 +53,9 @@ Error DiscoverBorderAgent(BorderAgentHandler aBorderAgentHandler, size_t aTimeou
     int socket = mdns_socket_open_ipv4();
     VerifyOrExit(socket >= 0, error = ERROR_IO_ERROR("failed to open mDNS IPv4 socket"));
 
-    if (aNetIf != "" && setsockopt(socket, SOL_SOCKET, SO_BINDTODEVICE, &aNetIf[0], sizeof(aNetIf)) < 0)
+    if (!aNetIf.empty() && setsockopt(socket, SOL_SOCKET, SO_BINDTODEVICE, &aNetIf[0], sizeof(aNetIf)) < 0)
     {
-         ExitNow(error = ERROR_SOCKET_BIND_ERROR("failed to bind network interface: {}", aNetIf));
+         ExitNow(error = ERROR_SOCKET_BIND_ERROR("failed to bind network interface {}: {}", aNetIf, strerror(errno)));
     }
 
     if (mdns_query_send(socket, kMdnsQueryType, kServiceName, strlen(kServiceName), buf, sizeof(buf)) != 0)
