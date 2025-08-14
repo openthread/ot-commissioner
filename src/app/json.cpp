5f343fa570f8eeb442b4f1343ed99e4374fadc82
pweiber <periclesweiber@ciandt.com>
2025-08-14 16:12:49 -0300

    feat: adding timeout diag tlv to json
    Signed-off-by: pweiber <periclesweiber@ciandt.com>

--- src/app/json.cpp
+++ src/app/json.cpp
@@ -779,6 +779,7 @@ static void to_json(Json &aJson, const NetDiagData &aNetDiagData)
 
     SET_IF_PRESENT(ExtMacAddr);
     SET_IF_PRESENT(MacAddr);
+    SET_IF_PRESENT(Timeout);
 #undef SET_IF_PRESENT
     if (aNetDiagData.mPresentFlags & NetDiagData::kMacCountersBit)
     {
