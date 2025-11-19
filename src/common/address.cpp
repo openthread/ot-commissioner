ab79410a53695eccc769fe6b0a3ce573ec0a66a1
Doug Ferraz <rochaferraz@google.com>
2025-11-19 18:40:28 +0800

    chg: add zero-padding for RLOC16 addresses
    Adds zero-padding to RLOC16 addresses to ensure they are always 4
    characters long. This fixes an issue where short RLOC16 addresses
    were not being handled correctly.
    
    Adds a new integration test to verify the fix.

--- src/common/address.cpp
+++ src/common/address.cpp
@@ -77,6 +77,11 @@ Error Address::Set(const std::string &aIp)
             }
 
             ByteArray rloc16Bytes;
+
+            // pad with zeros if necessary
+            for (size_t i = hexStr.size(); i < 4; i++) {
+                hexStr.insert(0, "0");
+            }
             error = utils::Hex(rloc16Bytes, hexStr);
             if (error == ErrorCode::kNone && rloc16Bytes.size() == kRloc16Size)
             {
