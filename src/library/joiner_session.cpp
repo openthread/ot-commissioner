91a7ca776b4f2a7f09435ae0194b47da823a11e2
Doug Ferraz <rochaferraz@google.com>
2026-02-19 23:31:46 -0500

    Refactor: JoinerSession - Fix dropping of oversized packets in proxy mode

--- src/library/joiner_session.cpp
+++ src/library/joiner_session.cpp
@@ -379,6 +379,7 @@ int JoinerSession::RelaySocket::Receive(uint8_t *aBuf, size_t aMaxLen, uint16_t 
             {
                 LOG_WARN(LOG_REGION_JOINER_SESSION, "session(={}) insufficient buffer size {}, {} needed",
                          static_cast<void *>(&mJoinerSession), aMaxLen, buf.size());
+                mRecvBufs.pop();
                 return MBEDTLS_ERR_SSL_BUFFER_TOO_SMALL;
             }
             rval = aMaxLen;
